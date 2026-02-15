dc872b521ea6233982a29c129c97b1a615e07413
Anna-Dinius <ae.dinius03@gmail.com>
2026-02-14 23:44:58 -0500

    Updated `server.js` to use `errorHandler`; updated `schemas/index.js` to import & export auth schemas

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const errorHandler = require('./src/middleware/errorHandler');
+
 // Load environment variables
 require('dotenv').config();
 
@@ -62,6 +64,8 @@ app.get('/', (req, res) => {
 app.use((req, res, next) => {
 	res.status(404).json({ error: 'Route not found' });
 });
+
+app.use(errorHandler);
 
 // Start the server and listen on the specified port
 const PORT = process.env.PORT || 5000;
